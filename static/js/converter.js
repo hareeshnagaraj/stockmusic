3c66ef6d26bf95eacf73515816048ac52cfda165
RyanBeatty <rvbeatty@email.wm.edu>
2014-09-14 03:48:24 -0400

    improved note bin packing algorithm

--- static/js/converter.js
+++ static/js/converter.js
@@ -3,19 +3,16 @@ function Converter(data,callback) {
 	console.log(raw_data[0]);
 	this.notes = [];
 
-	// console.log(raw_data)
 	var max = find_max(raw_data)
-	this.notes[0] = 60;
-	for( var i = 1; i < raw_data.length; i++){
-		// console.log(raw_data[i])
-		var next_note = raw_data[i]['close'] * 80 / max;
+	var min = find_min(raw_data)
+	for( var i = 0; i < raw_data.length; i++){
+		var next_note = (raw_data[i]['close']-min) * 128 / (max-min);
 
 		console.log(next_note)
 		this.notes.push(Math.round(next_note));
 		if( i == raw_data.length - 1 ){
 			callback(["finished",this.notes]);
 		}
-		// console.log(i)
 	}
 
 }
@@ -28,6 +25,21 @@ function calculate_change(cur_item, prev_item) {
 	var d_time = (Date.parse(cur_item['time']) - Date.parse(prev_item['time']))/86400000;
 	return d_price / d_time;
 }
+
+function find_min(data) {
+	if(data.length <= 0)
+		return undefined
+
+	var min = data[0]['close']
+	for(var i = 1; i < data.length; i++) {
+		if(data[i]['close'] < min) {
+			min = data[i]['close']
+		}
+	}
+
+	return min
+}
+
 
 function find_max(data) {
 	if (data.length <= 0)
